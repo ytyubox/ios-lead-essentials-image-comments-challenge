3fc7c17574539d6146e538e98e7a03d74b7b491e
Tsungyu Yu <ytyubox@gmail.com>
2021-02-03 16:25:10 +0800

    Refactor FeedLoader from fix [FeedImage] into Generic type

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -5,7 +5,48 @@
 import Foundation
 
 public protocol FeedLoader {
-	typealias Result = Swift.Result<[FeedImage], Error>
-	
-	func load(completion: @escaping (Result) -> Void)
+    associatedtype Output: ExpressibleByArrayLiteral
+    typealias Outcome = Swift.Result<Output, Error>
+    typealias Promise = (Outcome) -> Void
+
+    func load(completion: @escaping Promise)
+}
+
+public protocol EmptyDefault {
+    static var empty: Self { get }
+}
+
+private class FeedLoaderBaseBox<Output: ExpressibleByArrayLiteral>: FeedLoader {
+    init() {}
+    func load(completion _: @escaping Promise) {
+        fatalError()
+    }
+}
+
+private final class FeedLoaderBox<FeedLoaderType: FeedLoader>: FeedLoaderBaseBox<FeedLoaderType.Output> {
+    let base: FeedLoaderType
+    init(base: FeedLoaderType) {
+        self.base = base
+        super.init()
+    }
+
+    override func load(completion: @escaping Promise) {
+        base.load(completion: completion)
+    }
+}
+
+struct AnyFeedLoader<Output: ExpressibleByArrayLiteral>: FeedLoader {
+    private let box: FeedLoaderBaseBox<Output>
+
+    init<F: FeedLoader>(_ future: F) where Output == F.Output {
+        if let earsed = future as? AnyFeedLoader<Output> {
+            box = earsed.box
+        } else {
+            box = FeedLoaderBox(base: future)
+        }
+    }
+
+    func load(completion: @escaping Promise) {
+        box.load(completion: completion)
+    }
 }
